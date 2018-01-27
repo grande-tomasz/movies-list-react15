81eba6c89057130f91942d39d20969649f64dff7
Tomasz Grande <grande.tomasz@gmail.com>
2018-01-27 20:47:48 +0100

    Add movies posters and site styles

--- script.js
+++ script.js
@@ -2,19 +2,28 @@ var movies = [
   {
     id: 1,
     title: "Harry Potter",
-    desc: "Film o czarodzieju"
+    desc: "Film o czarodzieju",
+    src: "http://www.impawards.com/2007/posters/harry_potter_and_the_order_of_the_phoenix_ver3.jpg"
   },
   {
     id: 2,
     title: "Król Lew",
-    desc: "Film o królu sawanny"
+    desc: "Film o królu sawanny",
+    src: "http://www.impawards.com/1994/posters/lion_king_ver3.jpg"
+  },
+  {
+    id: 3,
+    title: "Matrix",
+    desc: "Film o alternatywnej wizji rzeczywistości",
+    src: "http://www.impawards.com/1999/posters/matrix_ver1.jpg"
   }
 ];
 
 var moviesElements = movies.map(function(movie) {
   return React.createElement("li", {key: movie.id},
     React.createElement("h2", {}, movie.title),
-    React.createElement("p", {}, movie.desc)
+    React.createElement("p", {}, movie.desc),
+    React.createElement("img", {src: movie.src},)
   );
 });
 
