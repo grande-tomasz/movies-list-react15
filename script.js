00fc640adca108699f7f3b9be89676d95ba721af
Tomasz Grande <grande.tomasz@gmail.com>
2018-01-27 23:19:31 +0100

    Create classes for different movie's elements

--- script.js
+++ script.js
@@ -3,41 +3,83 @@ var movies = [
     id: 1,
     title: "Harry Potter",
     desc: "Film o czarodzieju",
-    src: "http://www.impawards.com/2007/posters/harry_potter_and_the_order_of_the_phoenix_ver3.jpg"
+    src: "https://www.harrypottermovieposters.com/wp-content/uploads/2013/08/harry-potter-and-the-order-of-the-phoenix-movie-poster-style-h.jpg"
   },
   {
     id: 2,
     title: "Król Lew",
     desc: "Film o królu sawanny",
-    src: "http://www.impawards.com/1994/posters/lion_king_ver3.jpg"
+    src: "https://www.movieposter.com/posters/archive/main/108/MPW-54063"
   },
   {
     id: 3,
     title: "Matrix",
     desc: "Film o alternatywnej wizji rzeczywistości",
-    src: "http://www.impawards.com/1999/posters/matrix_ver1.jpg"
+    src: "https://images-na.ssl-images-amazon.com/images/I/51k1epcewKL.jpg"
+    // looks like src with underscore (_) does not work with nested React elements
+    // src: "http://www.impawards.com/1999/posters/matrix_ver1.jpg"
   }
 ];
 
-var moviesElements = movies.map(function(movie) {
-  return React.createElement("li", {key: movie.id},
-    React.createElement("h2", {}, movie.title),
-    React.createElement("p", {}, movie.desc),
-    React.createElement("img", {src: movie.src},)
-  );
+var MovieTitle = React.createClass({
+  propTypes: {
+    title: React.PropTypes.string.isRequired
+  },
+  render: function() {
+    return React.createElement("h2", {}, this.props.title);
+  }
+});
+var MovieDescription = React.createClass({
+  propTypes: {
+    desc: React.PropTypes.string.isRequired
+  },
+  render: function() {
+    return React.createElement("p", {}, this.props.desc);
+  }
+});
+var MovieImage = React.createClass({
+  propTypes: {
+    src: React.PropTypes.string.isRequired
+  },
+  render: function() {
+    return React.createElement("img", {src: this.props.src});
+  }
+});
+var Movie = React.createClass({
+  propTypes: {
+    movie: React.PropTypes.object.isRequired
+  },
+  render: function() {
+    return React.createElement("li", {key: this.props.movie.id},
+      React.createElement(MovieTitle, {title: this.props.movie.title}),
+      React.createElement(MovieDescription, {desc: this.props.movie.desc}),
+      React.createElement(MovieImage, {src: this.props.movie.src})
+    );
+  }
+});
+var MoviesList = React.createClass({
+  propTypes: {
+    movies: React.PropTypes.array.isRequired
+  },
+  render: function() {
+    return React.createElement("ul", {}, 
+      this.props.movies.map(function(movie) {
+        return React.createElement(Movie, {movie: movie});
+      })
+    );
+  }
 });
 
-var element = React.createElement(
-  "div",
-  {},
+var element = React.createElement("div", {},
   React.createElement("h1", {}, "Lista filmów"),
-  React.createElement("ul", {}, moviesElements)
-    // React.createElement("li", {},
-    //   React.createElement("h2", {}, "Król Lew"),
-    //   React.createElement("p", {}, "Film opowiadający historię króla sawanny")
+  React.createElement(MoviesList, {movies: movies})
 );
 
 ReactDOM.render(
   element,
   document.getElementById("app")
 );
+
+// in the console there is still:
+// Warning: Each child in an array or iterator should have a unique "key" prop
+// but do not know where this additional key should be defined
